57250ff86ef61e85e124ad5a3011b3477ae3cd12
hrjayasuryasingh9 <hrjayasuryasingh@gmail.com>
2025-04-15 11:44:33 +0530

    error rectification

--- Stepzo/Backend/src/Controllers/userController.js
+++ Stepzo/Backend/src/Controllers/userController.js
@@ -250,7 +250,7 @@ const generateJWT = (email, userID, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
   });
   return token;
 };
